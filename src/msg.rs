8b1fa1f28a9842f90ca4fa347b47e190afa6d57a
Metin Demir <metmirr@gmail.com>
2023-05-19 09:39:27 +0300

    feat: add json schema generation support

--- src/msg.rs
+++ src/msg.rs
@@ -1,29 +1,32 @@
+use cosmwasm_schema::{cw_serde, QueryResponses};
 use cosmwasm_std::Addr;
-use serde::{Deserialize, Serialize};
 
-#[derive(Serialize, Deserialize, Debug, PartialEq, Clone)]
+#[cw_serde]
 pub struct GreetResp {
     pub message: String,
 }
 
-#[derive(Serialize, Deserialize, Debug, PartialEq, Clone)]
+#[cw_serde]
+#[derive(QueryResponses)]
 pub enum QueryMsg {
+    #[returns(GreetResp)]
     Greet {},
+    #[returns(AdminListResp)]
     AdminList {},
 }
 
-#[derive(Debug, Deserialize, Serialize, PartialEq, Clone)]
+#[cw_serde]
 pub struct InstantiateMsg {
     pub admins: Vec<String>,
     pub donation_denom: String,
 }
 
-#[derive(Debug, Deserialize, Serialize, PartialEq, Clone)]
+#[cw_serde]
 pub struct AdminListResp {
     pub admins: Vec<Addr>,
 }
 
-#[derive(Debug, Deserialize, Serialize, PartialEq, Clone)]
+#[cw_serde]
 pub enum ExecuteMsg {
     AddMembers { admins: Vec<String> },
     Leave {},
